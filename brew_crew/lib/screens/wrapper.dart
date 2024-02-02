0b77a0bfcb626e49570ed13020712408ba3b8111
Imad Selka <imadselkawi133@gmail.com>
2024-02-02 22:57:13 +0100

    some changes

--- brew_crew/lib/screens/wrapper.dart
+++ brew_crew/lib/screens/wrapper.dart
@@ -1,4 +1,4 @@
-import 'package:brew_crew/screens/home/home.dart';
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
@@ -7,6 +7,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return home or authenticate widget
-    return const Home();
+    return const Authenticate();
   }
 }
