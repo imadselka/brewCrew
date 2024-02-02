692b36257b792feecacce53f7783c8594aa950a4
Imad Selka <imadselkawi133@gmail.com>
2024-02-02 21:55:18 +0100

    add auth page

---
+++ brew_crew/lib/services/auth.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Latest version :
+  // 'FirebaseUser' with 'User'
+  // '_auth.onAuthStateChanged' with '_auth.authStateChanges()'
+  // 'Firestore' with 'FirebaseFirestore'
+  // 'document' with 'doc'
+  // 'setData' with 'set'
+  // FirebaseUser has been changed to User
+  // AuthResult has been changed to UserCredential
+  // GoogleAuthProvider.getCredential() has been changed to GoogleAuthProvider.credential()
+  // onAuthStateChanged which notifies about changes to the user's sign-in state was replaced with authStateChanges()
+  // currentUser() which is a method to retrieve the currently logged in user,
+  // was replaced with the property currentUser and it no longer returns a Future<FirebaseUser>
+
+  //sign in anonymous user
+
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return user;
+    } catch (e) {}
+  }
+  //sign in wth email and password
+
+  //register with email and password
+
+  // sign out
+}
