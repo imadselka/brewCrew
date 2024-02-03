8642edd9567fc099e9b2e6d57ad771e22205a669
Imad Selka <imadselkawi133@gmail.com>
2024-02-03 14:50:36 +0100

    refactor code fix issues add provider wrapper

--- brew_crew/lib/main.dart
+++ brew_crew/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:brew_crew/firebase_options.dart';
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,9 +20,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Wrapper(),
+    return StreamProvider<UserObj?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper(),
+      ),
     );
   }
 }
